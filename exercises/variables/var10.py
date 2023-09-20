7635f20394e8936828c7319faa204702368e75b8
mastercode5 <43855236+mastercode5@users.noreply.github.com>
2023-09-20 18:22:01 +0200

    extra

--- exercises/variables/var10.py
+++ exercises/variables/var10.py
@@ -3,3 +3,6 @@
 # Napisz program któwy pobierze dwie wartości od użytkownika , po czym wypisze ich sumę
 # uwaga ten kod nie jest sprawdzany przez sprawdzarkę, jeśli chcesz go odpalić naciśnij przycisk run (prawy górny róg) lub otwórz odpowiednią 
 # scieżkę w terminalu i wpisz python var10.py
+
+# jeśli zrobiłeś wszystkie zadania i chcesz małą zapowiedź następnej lekcji dodaj jeszcze jeden input który określi rodzaj działania
+# jakie zostanie wykonane na liczbach
